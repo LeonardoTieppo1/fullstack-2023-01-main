1edbee78b9369a49150336c56ff140861d56c3b6
LeonardoTieppo1 <leonardo.tieppo2@hotmail.com>
2023-05-30 20:04:59 -0300

    Atividade de full Stack

--- design-antd/src/ComponentsD.js
+++ design-antd/src/ComponentsD.js
@@ -1,4 +1,4 @@
-import { Input } from "antd";
+import { Input, List } from "antd";
 import React from "react";
 import { useState } from "react";
 import {Button} from "antd";
@@ -11,10 +11,17 @@ export const ComponentsD = (props) => {
     const listUser= () =>{
         alert(username)
     }
-   return  <div style={{marginTop:15,marginRight:15, display: "flex",flexDirection: "row"}}>            
-            <Input addonBefore="Username"placeholder="Insert your username" value={username}
+    const selecionarItem= (item) =>{
+        alert(item.target.id)
+    }
+   return  <div style={{marginTop:10,marginRight:10}}>            
+            
+            <div style={{display: "flex"}}><Input addonBefore="Username"placeholder="Insert your username" value={username}
             onChange={updateUsername}/>
             <Button onClick={listUser}>Display</Button>
+            </div>
+        <div>
+            <List header="Lista de Compras" footer="Selecione os item de compras" bordered dataSource={["Café","Açúcar","Sabão"]} renderItem={(item,idx) => (<List.Item onClick={selecionarItem}id={idx}>{item}</List.Item>)}/>
         </div>
-
+    </div>
 }
